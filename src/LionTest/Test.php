e2120af026bfebeae131255e5b2ad5a90bd61e59
Sleon4 <sergioleon4004@hotmail.com>
2023-11-18 21:14:30 -0500

    build: initial configuration files have been added

---
+++ src/LionTest/Test.php
@@ -0,0 +1,71 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace LionTest;
+
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use RuntimeException;
+
+abstract class Test extends TestCase
+{
+	private mixed $instance;
+    private ReflectionClass $reflectionClass;
+
+    /**
+     * Initializes the object to perform a reflection on a class
+     * */
+    protected function initReflection(mixed $instance): void
+    {
+        $this->instance = $instance;
+        $this->reflectionClass = new ReflectionClass($this->instance);
+    }
+
+    /**
+     * Gets the value of a private property of a class
+     * */
+    protected function getPrivateProperty(string $property): mixed
+    {
+        $customProperty = $this->reflectionClass->getProperty($property);
+        $customProperty->setAccessible(true);
+
+        return $customProperty->getValue($this->instance);
+    }
+
+    /**
+     * Remove all files in a defined path
+     * */
+    protected function rmdirRecursively(string $dir): void
+    {
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+
+            foreach ($objects as $object) {
+                if ($object != "." && $object != "..") {
+                	$path = $dir . '/' . $object;
+
+                    if (is_dir($path)) {
+                        $this->rmdirRecursively($path);
+                    } else {
+                        unlink($path);
+                    }
+                }
+            }
+
+            rmdir($dir);
+        }
+    }
+
+    /**
+     * Create folders from a defined path
+     * */
+    protected function createDirectory(string $directory): void
+    {
+        if (!is_dir($directory)) {
+            if (!mkdir($directory, 0777, true)) {
+                throw new RuntimeException("Could not create directory: {$directory}");
+            }
+        }
+    }
+}
